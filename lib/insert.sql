3ce5935d0cd55c013c33e10715419ab8b64cad21
Matt Im <matthew.seiim@gmail.com>
2020-05-28 19:33:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 18, 'male', 'brown', 'nice', 1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy', 19, 'male', 'black', 'evil', 1);
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Tabitha', 20, 'female', 'pink', 'nice', 1);
--- INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Segeant Brown', 45, 'male', 'blue', 0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Segeant Brown', 45, 'male', 'blue', 0);
 -- INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Melissa', 21, 'female', 'white', 1);
 -- -- INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Grinch', 30, 'male', 'beige', 0);
 -- INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Wendy', 22, 'female', 'purple', 1);
