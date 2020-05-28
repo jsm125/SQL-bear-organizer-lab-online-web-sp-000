430347a5b88d7c7bfa95834a3fc2d0ab20780c5e
Matt Im <matthew.seiim@gmail.com>
2020-05-28 19:07:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Mr. Chocolate', 18, 'male', 'brown', 'nice', 1);
-  ('Rowdy', 19, 'male', 'black', 'evil', 1),
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ('Rowdy', 19, 'male', 'black', 'evil', 1),
   ('Tabitha', 20, 'female', 'pink', 'nice', 1),
   ('Segeant Brown', 45, 'male', 'blue', 0),
   ('Melissa', 21, 'female', 'white', 1),
