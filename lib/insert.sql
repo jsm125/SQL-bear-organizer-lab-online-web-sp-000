d5d2c8a61ac7fd925cc58c54407d3cfd92ed529c
Matt Im <matthew.seiim@gmail.com>
2020-05-28 19:01:43 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name,age,gender,color,temperament,alive)
 VALUES
-  (Mr. Chocolate, 18, male, brown, nice, 1),
-  (Rowdy, 19, male, black, evil, 1),
+  ('Mr. Chocolate', 18, 'male', 'brown', 'nice', 1),
+  ('Rowdy', 19, 'male', 'black', 'evil', 1),
   (Tabitha, 20, female, pink, nice, 1),
   (Segeant Brown, 45, male, blue, 0),
   (Melissa, 21, female, white, 1),
